394c5edcbe5771559ebfecdf28ca0d677fe26164
иван кедров <ivkedrov@gmail.com>
2023-06-08 20:07:59 +0300

    Added new HW

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,64 @@
 import datetime
 from flask import Flask
+import random
+import os
+import re
 
 app = Flask(__name__)
 
 
-@app.route('/hello_word')
-def test_function():
-   pass
+# @app.route('/hello_word')
+# def test_function():
+
+#    return 'Привет, мир!'
 
 
-@app.route('/cars')
-def test_function():
-   pass
+# cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+# @app.route('/cars')
+# def test_function():
+
+#    global cars
+#    return (', '.join(map(str, cars)))
 
 
-@app.route('/cats')
-def test_function():
-   pass
+# cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+# @app.route('/cats')
+# def test_function():
+
+#    return random.choice(cats)
+#
+#
+# @app.route('/get_time/now')
+# def test_function():
+
+#    current_time = datetime.datetime.now()
+#    return f'Точное время: {current_time}'
+#
+#
+# @app.route('/get_time/future')
+# def test_function():
+#
+#    current_time_after_hour = datetime.datetime.now() + datetime.timedelta(hours=1)
+#    return f'Точное время через час будет {current_time_after_hour}'
+#
+#
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+#
+# def war_and_peace():
+#
+#     word = random.choice(open(BOOK_FILE).read().split()).strip()
+#     return re.sub(r'[^\w\s]', '', word)
+#
+# @app.route('/get_random_word')
+# def test_function():
+#
+#     return war_and_peace()
 
 
-@app.route('/get_time/now')
-def test_function():
-   pass
-
-
-@app.route('/get_time/future')
-def test_function():
-   pass
-
-
-@app.route('/get_random_word')
-def test_function():
-   pass
+# counter = 0
+# @app.route('/counter')
+# def test_function():
+#     global counter
+#     counter += 1
+#     return f'This page was opened {counter} times'
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 def test_function():
    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
                      datetime.datetime.now().utcnow()
+
+counter = 0
+@app.route('/hello_world')
+def hello_world():
+    global counter
+    counter += 1
+    return f'Hello world. This page was opened {counter} times'
