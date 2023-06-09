a4bc38810ffdaf738a56af1356e5f9d2a0cd80ea
иван кедров <ivkedrov@gmail.com>
2023-06-09 21:08:02 +0300

    Some changes in HW

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -7,58 +7,56 @@ import re
 app = Flask(__name__)
 
 
-# @app.route('/hello_word')
-# def test_function():
+@app.route('/hello_word')
+def test_function():
 
-#    return 'Привет, мир!'
-
-
-# cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
-# @app.route('/cars')
-# def test_function():
-
-#    global cars
-#    return (', '.join(map(str, cars)))
+   return 'Привет, мир!'
 
 
-# cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
-# @app.route('/cats')
-# def test_function():
+cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+@app.route('/cars')
+def test_function():
 
-#    return random.choice(cats)
-#
-#
-# @app.route('/get_time/now')
-# def test_function():
-
-#    current_time = datetime.datetime.now()
-#    return f'Точное время: {current_time}'
-#
-#
-# @app.route('/get_time/future')
-# def test_function():
-#
-#    current_time_after_hour = datetime.datetime.now() + datetime.timedelta(hours=1)
-#    return f'Точное время через час будет {current_time_after_hour}'
-#
-#
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
-#
-# def war_and_peace():
-#
-#     word = random.choice(open(BOOK_FILE).read().split()).strip()
-#     return re.sub(r'[^\w\s]', '', word)
-#
-# @app.route('/get_random_word')
-# def test_function():
-#
-#     return war_and_peace()
+   global cars
+   return (', '.join(map(str, cars)))
 
 
-# counter = 0
-# @app.route('/counter')
-# def test_function():
-#     global counter
-#     counter += 1
-#     return f'This page was opened {counter} times'
+cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+@app.route('/cats')
+def test_function():
+
+   return random.choice(cats)
+
+
+@app.route('/get_time/now')
+def test_function():
+
+   current_time = datetime.datetime.now()
+   return f'Точное время: {current_time}'
+
+
+@app.route('/get_time/future')
+def test_function():
+
+   current_time_after_hour = datetime.datetime.now() + datetime.timedelta(hours=1)
+   return f'Точное время через час будет {current_time_after_hour}'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+with open(BOOK_FILE) as book:
+    words = book.read().split()
+
+@app.route('/get_random_word')
+def test_function():
+
+    global words
+    word = random.choice(words)
+    return re.sub(r'[^\w\s]', '', word)
+
+counter = 0
+@app.route('/counter')
+def test_function():
+    global counter
+    counter += 1
+    return f'This page was opened {counter} times'
